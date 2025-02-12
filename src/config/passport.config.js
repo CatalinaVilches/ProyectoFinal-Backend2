c5b1d23777ce792a48c7c35006fc9444b83a7377
CatalinaVilches <catalina.vilchesf99@gmail.com>
2025-02-12 01:38:57 -0300

    Update passport.config.js

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,44 +1,44 @@
 import passport from 'passport';
 import local from 'passport-local';
 import jwt from 'passport-jwt';
-import userService from '../models/User.js';
+import userService from '../models/user.models.js';
 import { createHash, isValidPassword } from '../utils.js';
 
+const { PRIVATE_KEY } = process.env;
 
 const LocalStrategy = local.Strategy;
-
 const JWTStrategy = jwt.Strategy;
 const ExtractJWT = jwt.ExtractJwt;
 
 const cookieExtractor = (req) => {
     let token = null;
     if (req && req.cookies) {
-        token = req.cookies['tokenCookie']; 
+        token = req.cookies['tokenCookie'];
     }
     return token;
 };
 
-const initializePassport = (PRIVATE_KEY) => {
+const initializePassport = () => {
     passport.use('register', new LocalStrategy(
-        { passReqToCallback: true, usernameField: 'email' }, 
-        async (req, username, password, done) => { 
-            const { first_name, last_name, email, age } = req.body; 
+        { passReqToCallback: true, usernameField: 'email' },
+        async (req, username, password, done) => {
+            const { first_name, last_name, email, age } = req.body;
             try {
                 let user = await userService.findOne({ email: email });
                 if (user) {
                     console.log('User already exists');
-                    return done(null, false); 
+                    return done(null, false);
                 }
                 let newUser = {
-                    first_name, 
-                    last_name, 
-                    email, 
-                    age, 
-                    password: createHash(password) 
+                    first_name,
+                    last_name,
+                    email,
+                    age,
+                    password: createHash(password)
                 };
 
                 const userCreated = await userService.create(newUser);
-                return done(null, userCreated); 
+                return done(null, userCreated);
             } catch (error) {
                 return done(error);
             }
@@ -46,10 +46,10 @@ const initializePassport = (PRIVATE_KEY) => {
     ));
 
     passport.use('login', new LocalStrategy(
-        { passReqToCallback: true, usernameField: 'email' }, 
-        async (req, username, password, done) => { 
+        { passReqToCallback: true, usernameField: 'email' },
+        async (req, username, password, done) => {
             try {
-                const user = await userService.findOne({ email: username }); 
+                const user = await userService.findOne({ email: username });
                 if (!user) {
                     console.log('User doesnt exist');
                     return done(null, false, { message: 'Usuario no encontrado' });
@@ -58,7 +58,7 @@ const initializePassport = (PRIVATE_KEY) => {
                 if (!isValidPassword(user, password)) {
                     return done(null, false, { message: 'Contraseña incorrecta' });
                 }
-                return done(null, user); 
+                return done(null, user);
             } catch (error) {
                 return done(error);
             }
@@ -67,23 +67,21 @@ const initializePassport = (PRIVATE_KEY) => {
 
     passport.use('jwt', new JWTStrategy(
         {
-            jwtFromRequest: ExtractJWT.fromExtractors([cookieExtractor]), 
+            jwtFromRequest: ExtractJWT.fromExtractors([cookieExtractor]),
             secretOrKey: PRIVATE_KEY, 
         },
         async (jwt_payload, done) => {
             try {
-
-                const user = await userService.findById(jwt_payload.id); 
+                const user = await userService.findById(jwt_payload.id);
                 if (!user) {
                     return done(null, false, { message: 'Usuario no encontrado' });
                 }
-                return done(null, user); 
+                return done(null, user);
             } catch (error) {
                 done(error);
             }
         }
     ));
-    
 
     passport.serializeUser((user, done) => {
         done(null, user.id);
@@ -96,3 +94,4 @@ const initializePassport = (PRIVATE_KEY) => {
 };
 
 export default initializePassport;
+
